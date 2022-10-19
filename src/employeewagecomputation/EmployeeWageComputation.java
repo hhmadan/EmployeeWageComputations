ecd4b4ddbe0a80f301db7f369d040550cfb94a8e
hhmadan <hhmadan03@gmail.com>
2022-10-20 00:19:17 +0530

    Added: UC1 Check Employee is Present or Absent

--- src/employeewagecomputation/EmployeeWageComputation.java
+++ src/employeewagecomputation/EmployeeWageComputation.java
@@ -1,8 +1,20 @@
 //Displaying Welcome message on master branch
+//UC1 Check Emp is Present or Absent
 package employeewagecomputation;
 
+import java.util.Random;
+
 public class EmployeeWageComputation {
+
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+
+        if(!attendance())
+            System.out.println("Employee is Absent");
+        else
+            System.out.println("Employee is Present");
+    }
+    public static boolean attendance(){
+        return new Random().nextBoolean();
     }
 }
