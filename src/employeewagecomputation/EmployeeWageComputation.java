595d1fbd750229427558aa017c01672d0d84b5d3
hhmadan <hhmadan03@gmail.com>
2022-10-20 00:29:38 +0530

    Added: Daily Full day Employee Wage

--- src/employeewagecomputation/EmployeeWageComputation.java
+++ src/employeewagecomputation/EmployeeWageComputation.java
@@ -1,5 +1,6 @@
 //Displaying Welcome message on master branch
-//UC1 Check Emp is Present or Absent
+//UC2 Calculate Daily Wage of Employee
+
 package employeewagecomputation;
 
 import java.util.Random;
@@ -8,11 +9,18 @@ public class EmployeeWageComputation {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+        int wagePerHr = 20;
+        int fullDayHr = 8;
+        int dailyWage;
 
-        if(!attendance())
-            System.out.println("Employee is Absent");
-        else
+        if(attendance()) {
             System.out.println("Employee is Present");
+            dailyWage = wagePerHr * fullDayHr;
+            System.out.println("\nDaily wage of Employee is: "+ dailyWage);
+        }
+        else
+            System.out.println("Employee is Absent");
+
     }
     public static boolean attendance(){
         return new Random().nextBoolean();
