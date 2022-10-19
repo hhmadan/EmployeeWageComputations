cb83240f51ccb7ee143189f9d8cac2fa20c4d2d1
hhmadan <hhmadan03@gmail.com>
2022-10-19 23:35:29 +0530

    Added: Display Welcome message

---
+++ src/employeewagecomputation/EmployeeWageComputation.java
@@ -0,0 +1,8 @@
+//Displaying Welcome message on master branch
+package employeewagecomputation;
+
+public class EmployeeWageComputation {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program");
+    }
+}
